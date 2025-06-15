a8ad6991bfe6c5a6d45be8c21eab574bc4ca128c
TekknoTim <timmey1995@hotmail.de>
2025-06-15 14:39:10 +0200

    -

--- Program.cs
+++ Program.cs
@@ -4,9 +4,6 @@ namespace ZomboidBackupManager
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
